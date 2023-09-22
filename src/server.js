54ee8c05c6241a09aae72a190ad17a93ea33aa68
ThaiHero <lnt0995449235@gmail.com>
2023-09-22 15:38:21 +0700

    update database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require("path")
 const configviewEngine = require("./config/viewEngine.js")
 const webroutes = require("./routes/web.js")
+const mysql = require("mysql2");
+
 require("dotenv").config();
 
 
@@ -14,6 +16,21 @@ configviewEngine(app)
 
 // config router
 app.use("/", webroutes);
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    password: '123456',
+    user: 'root',
+    database: 'hoidanit'
+})
+
+connection.query(
+    'select * from User where id=1',
+    function (err, result, fields) {
+        console.log(result);
+    }
+)
 
 app.listen(port, hostname, () => {
     console.log("Listening on port " + port)
