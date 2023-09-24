607435966613e246fb7a8436b74e574020f71054
ThaiHero <lnt0995449235@gmail.com>
2023-09-24 14:01:41 +0700

    update post query and save in database

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -3,19 +3,19 @@ const connection = require("../config/database.js");
 
 const getHomeText = (req, res) => {
 
-    // let users = []
-    // connection.query(
-    //     'select * from User where id=1',
-    //     function (err, result, fields) {
-    //         if (!err) {
-    //             users = result;
-    //             console.log("Inside: ", users);
-    //             //res.writeHead(200, "content-type", "text/plain");
-    //             res.send(JSON.stringify(users));
-    //         }
-    //     }
-    // )
-    // console.log("Outside: ", users);
+    let users = []
+    connection.query(
+        'select * from User where id=2',
+        function (err, result, fields) {
+            if (!err) {
+                users = result;
+                console.log("Inside: ", users);
+                //res.writeHead(200, "content-type", "text/plain");
+                //res.send(JSON.stringify(users));
+            }
+        }
+    )
+    console.log("Outside: ", users);
     return res.render("Homepage.ejs")
 }
 
@@ -24,8 +24,22 @@ const getHomePic = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    res.send("Created successfull")
-    console.log(req.body);
+    let { email, name, city } = req.body;
+    console.log("email=", email)
+    console.log("name=", name)
+    console.log("city=", city)
+    connection.query(
+        `insert into User(email,name,city)
+        values (?, ?, ?)`,
+        [email, name, city],
+        function (err, result) {
+            //console.log(result);
+            res.send("Add user successfull!");
+            if (err) {
+                console.log(err);
+            }
+        }
+    )
 }
 
 
